7449dc5a717e190d113de5a5a96cee1bad57077a
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-10-03 14:01:06 +0000

    feat(toasts): add a toaster - svelte-sonner

---
+++ packages/client/src/lib/comps/ui/Toasts.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // @TODO: at the moment the toast function works with toastState that is signletone,
+  // we try not to use signletones in Sila and instead pack everything in
+  // clientState that lives in a shared context. So we would need to refactor
+  // toasts at some point to use clientState as well.
+  // Perhaps make our own version of https://github.com/wobsoriano/svelte-sonner/
+  import { Toaster, toast } from "svelte-sonner";
+  import { useClientState } from "@sila/client/state/clientStateContext";
+  import { onMount } from "svelte";
+ 
+  onMount(() => {
+    toast.success("Yaaaay!", {
+      dismissable: true,
+      duration: Infinity,
+      closeButton: true,
+    });
+
+    toast("This is a regular toast!");
+    toast.success("Operation completed successfully.");
+    toast.error("Something went wrong.");
+    toast.info("Here is some information.");
+    toast.warning("This is a warning toast.");
+  });
+</script>
+
+<Toaster position="top-right" />
