d9116ae82dbbbb6ee104b67ac7397641e2af97d5
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-10-22 14:14:18 +0000

    docs: remove comments

--- packages/core/src/apps/ChatAppBackend.ts
+++ packages/core/src/apps/ChatAppBackend.ts
@@ -9,13 +9,7 @@ import { ThreadMessage } from "../models";
 export default class ChatAppBackend {
   private data: ChatAppData;
   private agentServices: AgentServices;
-
   private defaultChatAgent: WrapChatAgent;
-
-  // @TODO: should I have 3 agents here and run them in parallel or should the main agent invoke those?
-  // 1. the main agent replies to messages
-  // 2. the title agent updates the title
-  // 3. the summarizer agent summarizes conversations by adding summary messages
 
   get appTreeId(): string {
     return this.appTree.tree.root!.id;
