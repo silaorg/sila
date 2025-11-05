d9450897401d87548867dfe9b7d300df40bc9f73
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-11-05 10:38:51 +0000

    fix: import of ClientStateProvider

--- packages/desktop/src/DesktopApp.svelte
+++ packages/desktop/src/DesktopApp.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { ClientState, SilaApp, type ClientStateConfig } from "@sila/client";
+  import { ClientState, SilaApp, ClientStateProvider, type ClientStateConfig } from "@sila/client";
   import { electronFsWrapper } from "./electronFsWrapper";
   import { electronDialogsWrapper } from "./electronDialogsWrapper";
-  import ClientStateProvider from "@sila/client/state/ClientStateProvider.svelte";
   import DesktopUpdateHandler from "./DesktopUpdateHandler.svelte";
 
   let config: ClientStateConfig | null = $state({
