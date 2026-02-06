ca865089799ead659d4001e1dd0037a4a7f6c2b9
Dmitry Kury <dima.kurilchenko@gmail.com>
2026-02-06 11:34:36 +0000

    feat(client): implement stacked loading state with fade out

---
+++ packages/client/src/lib/comps/basic/GlobalLoading.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { ProgressRing } from "@skeletonlabs/skeleton-svelte";
+  import { fade } from "svelte/transition";
+  import { globalLoading } from "../../state/loading.svelte";
+</script>
+
+{#if globalLoading.count > 0}
+  <div
+    out:fade={{ duration: 200, delay: 300 }}
+    class="fixed inset-0 flex items-center justify-center z-50 bg-surface-50-950"
+  >
+    <ProgressRing value={null} />
+  </div>
+{/if}
