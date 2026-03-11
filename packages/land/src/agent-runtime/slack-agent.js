96532ad395eae765230861cf0e3025840baaefad
Dmitry Kury <dima.kurilchenko@gmail.com>
2026-03-11 12:07:50 +0000

    dev: remove old land package path

--- packages/land/src/agent-runtime/slack-agent.js
+++
@@ -1,7 +0,0 @@
-import { createChatAgent } from "./chat-agent.js";
-
-export { createChatAgent };
-
-export function createSlackChatAgent(lang, options) {
-  return createChatAgent(lang, options);
-}
